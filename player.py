a3a39311dee8e79f37c3d18083b5e1d0ac2a4754
Fuzzwah <rob.crouch@gmail.com>
2015-02-16 18:59:02 -0700

    rolled back some changes to player

--- player.py
+++ player.py
@@ -3,8 +3,25 @@ from threading import Thread
 from Queue import Queue
 import os
 import util
+import config as cfg
 from main import ServerStatus
 
+############################################################
+### MASSIVE AMOUNTS OF CONFIG (this should probably be in a DB somewhere)
+############################################################
+defaultPlayer = ["mplayer"]
+
+playerTable = {}
+
+commandTable = {
+    'mplayer':
+        {'step-backward': "\x1B[B", 'backward': "\x1B[D", 'forward': "\x1B[C", 'step-forward': "\x1B[A",
+         ## down | left | right | up
+         'volume-down': "9", 'volume-off': "m", 'volume-up': "0",
+         'stop': "q", 'pause': " ", 'play': " "},
+    }
+### END THE MASSIVE CONFIG
+############################################################
 try:
     commandQueue ## Global multi-process queue to accept player commands
     playQ        ## Global multi-process queue to accept files to play
@@ -17,16 +34,13 @@ def listen():
         aFile = playQ.get()
         if util.isInRoot(aFile):
             ServerStatus.send(util.nameToTitle(aFile), event='playing')
-            playerCmd = "mplayer"
-            cmdTable = {'step-backward': "\x1B[B", 'backward': "\x1B[D", 'forward': "\x1B[C", 'step-forward': "\x1B[A",
-						 ## down | left | right | up
-						 'volume-down': "9", 'volume-off': "m", 'volume-up': "0",
-						 'stop': "q", 'pause': " ", 'play': " "}
+            playerCmd = __getPlayerCommand(aFile)
+            cmdTable = commandTable[playerCmd[0]]
             playFile(playerCmd, aFile, cmdTable)
 
 def playFile(playerCmd, fileName, cmdTable):
     __clearQueue(commandQueue)
-    activePlayer = Popen(playerCmd + fileName, stdin=PIPE)
+    activePlayer = Popen(playerCmd + [fileName], stdin=PIPE)
     while activePlayer.poll() == None:
         try:
             res = commandQueue.get(timeout=1)
@@ -57,6 +71,12 @@ def playList(playerCmd, fileName, cmdTable):
             None
     ServerStatus.send(util.nameToTitle(fileName), event="finished")
     return True    
+
+### Local Utility
+def __getPlayerCommand(filename):
+    global playerTable, defaultPlayer
+    name, ext = os.path.splitext(filename)
+    return playerTable.get(ext[1:], defaultPlayer)
 
 def __clearQueue(q):
     while not q.empty():
